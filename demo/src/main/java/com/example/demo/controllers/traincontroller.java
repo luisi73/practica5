546fee57bcce945f4c2c2dd3212ad2886ed677df
Luis Bueno Archaga <luisibien@gmail.com>
2022-03-14 22:11:28 +0100

    main

--- demo/src/main/java/com/example/demo/controllers/traincontroller.java
+++
@@ -1,7 +0,0 @@
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.RestController;
-
-public class traincontroller {
-    
-}
