e258d8024ba57978f70b28911aad178614b9e25f
Luis Bueno Archaga <luisibien@gmail.com>
2022-03-15 12:57:06 +0100

    main

--- demo/src/main/java/com/example/Clientes.java
+++ demo/src/main/java/com/example/Clientes.java
@@ -4,9 +4,15 @@ public class Clientes {
     private String nombre;
     private String correo;
     private Number numero;
-
+    private boolean trato;
     public String getNombre() {
         return nombre;
+    }
+    public boolean isTrato() {
+        return trato;
+    }
+    public void setTrato(boolean trato) {
+        this.trato = trato;
     }
     public Number getNumero() {
         return numero;
